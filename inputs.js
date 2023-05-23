44fbc1db5202d7a95a7ec0b34b97a4138f733ad2
MayanLasker <mayanmiriam@gmail.com>
2023-05-23 20:45:11 +0300

    class10-inputs

---
+++ inputs.js
@@ -0,0 +1,30 @@
+let userName=document.getElementById('userName')
+userName.onchange=()=>{console.log(userName.value);}
+
+let userPassword=document.getElementById('userPassword')
+userPassword.onchange=()=>{console.log(userPassword.value);}
+
+let userTel=document.getElementById('userTel')
+userTel.onchange=()=>{console.log(userTel.value);}
+
+let userMail=document.getElementById('userMail')
+userMail.onchange=()=>{console.log(userMail.value);}
+
+let userBirth=document.getElementById('userBirth')
+userBirth.onchange=()=>{console.log(userBirth.value);}
+
+let userFamily=document.getElementById('userFamily')
+userFamily.onchange=()=>{console.log(userFamily.value);}
+
+let userColor=document.getElementById('userColor')
+userColor.onchange=()=>{console.log(userColor.value);}
+
+let doUser=document.getElementById('doUser')
+doUser.onchange=()=>{console.log(doUser.value);}
+
+let btn=document.getElementById('btn')
+btn.onclick=()=>{alert("הטופס נשלח בהצלחה!")}
+
+
+
+
